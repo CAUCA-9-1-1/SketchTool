b70c4c4c3410bcb73ee1ed43ee9a9ae435c4bf5f
audrey <audrey.lebelr@gmail.com>
2018-06-14 09:09:15 -0400

    Added project files

---
+++ projects/lib-sketch-tool/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of sketch-tool
+ */
+
+export * from './lib/services/canvas-manager.service';
+export * from './lib/canvas-sketch-manager.component';
+export * from './lib/canvas-sketch-manager.module';
