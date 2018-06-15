16c603500d4ebfb28319d1e432ee74ff72e968d0
audrey <audrey.lebelr@gmail.com>
2018-06-14 22:55:05 -0400

    Update demo

--- projects/lib-sketch-tool/src/lib/sketch-tool.model.ts
+++ projects/lib-sketch-tool/src/lib/sketch-tool.model.ts
@@ -1,3 +1,4 @@
-export * from './services/canvas-manager.service';
+/* export * from './services/canvas-manager.service'; */
 export * from './modules/web-sketch-tool.module';
+export * from './modules/mobile-sketch-tool.module';
 export * from './sketch-tool.model';
--- projects/lib-sketch-tool/src/public_api.ts
+++ projects/lib-sketch-tool/src/public_api.ts
@@ -2,6 +2,8 @@
  * Public API Surface of sketch-tool
  */
 
-export * from './lib/services/canvas-manager.service';
+/* export * from './lib/services/canvas-manager.service'; */
 export * from './lib/web-component/web-sketch-tool.component';
+export * from './lib/mobile-component/mobile-sketch-tool.component';
 export * from './lib/modules/web-sketch-tool.module';
+export * from './lib/modules/mobile-sketch-tool.module';
