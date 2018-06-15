7779d486e375eb5af786d644515ea7b458743fe3
audrey <audrey.lebelr@gmail.com>
2018-06-15 15:54:49 -0400

    Fix for compatibility

--- projects/lib-sketch-tool/src/lib/sketch-tool.model.ts
+++
@@ -1,4 +0,0 @@
-/* export * from './services/canvas-manager.service'; */
-export * from './modules/web-sketch-tool.module';
-export * from './modules/mobile-sketch-tool.module';
-export * from './sketch-tool.model';
