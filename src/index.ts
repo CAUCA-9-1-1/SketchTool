2e440b688f944e98c876b86d80a0f45b652649f3
audrey <audrey.lebelr@gmail.com>
2018-06-15 15:54:49 -0400

    Fix for compatibility

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from './web-component/web-sketch-tool.component';
+export * from './mobile-component/mobile-sketch-tool.component';
+export * from './modules/web-sketch-tool.module';
+export * from './modules/mobile-sketch-tool.module';
