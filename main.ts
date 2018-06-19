32c3b925c0b3e145f04597efa9a5039704952c9d
audrey <audrey.lebelr@gmail.com>
2018-06-19 01:17:50 -0400

    Fixed dependecies warnings

--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './example/app/app.module';
+
+import { SketchToolModule } from './src/modules/sketch-tool.module';
 import { environment } from './config/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(SketchToolModule)
   .catch(err => console.log(err));
