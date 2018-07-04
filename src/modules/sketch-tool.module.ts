4c05be3cd069a7e786d5712232a21f38f46bab70
audrey <audrey.lebelr@gmail.com>
2018-07-04 15:18:26 -0400

    Updated dependencies

--- src/modules/sketch-tool.module.ts
+++ src/modules/sketch-tool.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-// import {ColorPickerModule} from 'narik-angular-color-picker';
-
 import { MobileSketchToolComponent } from './../mobile-component/mobile-sketch-tool.component';
 import { WebSketchToolComponent } from './../web-component/web-sketch-tool.component';
 import { TestCanvasClickComponent } from './../directives/canvas-click.directive.spec';
