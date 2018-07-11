19824d65f1966e1fb535a985488dc97a23ae4de6
audrey <audrey.lebelr@gmail.com>
2018-07-11 19:44:09 -0400

    Web component v1

---
+++ src/app/lib-sketch-tool/web-component/web-sketch-tool.component.spec.ts
@@ -0,0 +1,23 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+
+/* import { ColorPickerModule } from 'narik-angular-color-picker'; */
+import { WebSketchToolComponent } from './web-sketch-tool.component';
+
+describe('InspectionSketchToolComponent', () => {
+  let component: WebSketchToolComponent;
+  let fixture: ComponentFixture<WebSketchToolComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [WebSketchToolComponent],
+      imports: [FormsModule]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WebSketchToolComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+});
