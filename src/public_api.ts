44fa75770e867aea21cb22ffcf9ade65ce74bb5d
audrey <audrey.lebelr@gmail.com>
2018-07-25 16:33:22 -0400

    Updated to version 1.1.3 To deal with json object as input

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,1 +1,3 @@
+export * from './app/lib-sketch-tool/mobile-component/mobile-sketch-tool.component';
+export * from './app/lib-sketch-tool/services/canvas-manager.service';
 export * from './app/lib-sketch-tool/sketch-tool.module';
