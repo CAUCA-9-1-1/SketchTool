ee59fdafdde00253eabd5b1c3411a76b81f7d5d3
audrey <audrey.lebelr@gmail.com>
2018-08-14 09:35:14 -0400

    feat(#5) Update to font-awesome 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { MatSelectModule } from '@angular/material/select';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { ColorPickerModule } from 'ngx-color-picker';
-import { MatButtonModule } from '@angular/material';
 
-import { WebSketchToolComponent } from './lib-sketch-tool/web-component/web-sketch-tool.component';
+import { WebSketchToolModule } from './lib-sketch-tool/web-sketch-tool.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WebSketchToolComponent
   ],
   imports: [
     BrowserModule,
-    MatSelectModule,
-    ColorPickerModule,
-    MatButtonModule,
-    MatToolbarModule,
+    WebSketchToolModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
