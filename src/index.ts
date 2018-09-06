062fc1987441d9273762364aa74683ac7cd8ff0d
audrey <audrey.lebelr@gmail.com>
2018-09-06 10:41:59 -0400

    fix(#11) Remove imports of mobile module

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './app/lib-sketch-tool/mobile-component/mobile-sketch-tool.component';
-export * from './app/lib-sketch-tool/services/canvas-manager.service';
-export * from './app/lib-sketch-tool/sketch-tool.module';
-
