5fafc972ada0627657f0be0670106a6a2ad5fb0e
audrey <audrey.lebelr@gmail.com>
2018-12-13 22:06:00 -0500

    feat(#13): Added tooltips

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient, HttpClientModule  } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { WebSketchToolModule } from './lib-sketch-tool/web-sketch-tool.module';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +18,20 @@ import { WebSketchToolModule } from './lib-sketch-tool/web-sketch-tool.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    TranslateModule.forRoot(),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     WebSketchToolModule
 
   ],
+  exports: [TranslateModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/lib-sketch-tool/web-sketch-tool.module.ts
+++ src/app/lib-sketch-tool/web-sketch-tool.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ColorPickerModule } from 'ngx-color-picker';
 import { MatButtonModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { WebSketchToolComponent } from './web-component/web-sketch-tool.component';
 import { CanvasManagerService } from './services/canvas-manager.service';
 
 @NgModule({
-  imports: [CommonModule, ColorPickerModule, MatButtonModule, MatToolbarModule],
+  imports: [CommonModule, ColorPickerModule, MatButtonModule, MatToolbarModule, NoopAnimationsModule, MatTooltipModule],
   declarations: [WebSketchToolComponent],
   providers: [CanvasManagerService],
   exports: [WebSketchToolComponent]
 })
-export class WebSketchToolModule {}
+export class WebSketchToolModule { }
