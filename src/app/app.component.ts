c002c224b97c0122a4ebc0cb7d8a4869b90033d9
audrey <audrey.lebelr@gmail.com>
2018-12-18 17:50:06 -0500

    feat(#13): Updated example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  public imageUrl = 'assets/implentation-plan-example-picture.png';
+export class AppComponent implements AfterViewInit {
+  public imageUrl: string;
   public isShowingResult: boolean;
 
   private canvas;
 
-  constructor() {
+  constructor(translate: TranslateService) {
+    this.imageUrl = '';
     this.isShowingResult = false;
+
+    translate.setDefaultLang('en');
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.imageUrl = 'assets/implentation-plan-example-picture.png';
+    })
   }
 
   get resultJson(): string {
